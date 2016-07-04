96d64d0b788acc3edf85106bff7399d3033db6c4
Bodin Punyaprateep <bodin.punyaprateep@gmail.com>
2016-07-04 00:04:21 -0400

    Update main.py
    added tkdnd support

--- ExcelApp/main.py
+++ ExcelApp/main.py
@@ -1,7 +1,20 @@
 import tkinter as tk
 import MainWindow as m
+import os
+from tkdnd_wrapper import TkDND
 
 root = tk.Tk()
+dnd = TkDND(root)
 root.title("Excel Sheet Creator")
 application = m.MainApplication(root)
+
+def handle(event):
+    filelist = root.tk.splitlist(event.data)
+    application.main.inputlist = list(filelist)
+    filenames = []
+    for filename in filelist:
+        filenames.append(os.path.basename(filename))
+    application.main.fileinput.selectedfilenames.set(", ".join(filenames))
+
+dnd.bindtarget(application.main.fileinput.inputbox, handle, 'text/uri-list')
 root.mainloop()
