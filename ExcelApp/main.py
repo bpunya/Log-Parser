c443aa370f0116c6481b58d3bf1b2bf53ffc6442
Bodin Punyaprateep <bodin.punyaprateep@gmail.com>
2016-07-05 00:25:19 -0400

    Update main.py

--- ExcelApp/main.py
+++ ExcelApp/main.py
@@ -1,20 +1,10 @@
-import tkinter as tk
-import MainWindow as m
-import os
-from tkdnd_wrapper import TkDND
+import tkinter
+import MainWindow
 
-root = tk.Tk()
-dnd = TkDND(root)
+root = tkinter.Tk()
+root.resizable(width=False, height=False)
+root.geometry("635x320+100+100")
 root.title("Excel Sheet Creator")
-application = m.MainApplication(root)
-
-def handle(event):
-    filelist = root.tk.splitlist(event.data)
-    application.main.inputlist = list(filelist)
-    filenames = []
-    for filename in filelist:
-        filenames.append(os.path.basename(filename))
-    application.main.fileinput.selectedfilenames.set(", ".join(filenames))
-
-dnd.bindtarget(application.main.fileinput.inputbox, handle, 'text/uri-list')
+application = MainWindow.ApplicationWrapper(root)
+application.pack(fill="both", expand=True)
 root.mainloop()
