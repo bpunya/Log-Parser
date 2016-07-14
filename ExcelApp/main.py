4ccb79b24535bede29290da32e5e151ebc3d7449
Bodin Punyaprateep <bodin.punyaprateep@gmail.com>
2016-07-14 13:33:10 -0400

    showing object nesting

--- ExcelApp/main.py
+++ ExcelApp/main.py
@@ -8,3 +8,31 @@ root.title("Excel Sheet Creator")
 application = MainWindow.ApplicationWrapper(root)
 application.pack(fill="both", expand=True)
 root.mainloop()
+
+# Format of the nested classes is as follows:
+#
+# "Parent"
+# |
+# |-- "Child1"
+# |-- "Child2"
+#
+# root (TK instance)
+# |
+# |-- application (ApplicationWrapper)
+#     |
+#     |-- scrollbar (Scrollbar)
+#     |-- main (MainWindow)
+#         |-- changeFrame()
+#         |
+#         |-- msg (MessageFrame)
+#         |-- help (HelpFrame)
+#         |-- back (ReturnFrame)
+#         |-- main (MainFrame)
+#             |-- selectFile(), clearInputs(), handleTempalteInput(), handleFileInput()
+#             |
+#             |-- tcl (Tcl interpreter)
+#             |-- infoblock (InfoBlock) >>> Accesses ProgramMain.startProgram()
+#             |-- templateinput (TemplateInput)
+#             |-- fileinputheader (FileInputHeader)
+#             |-- fileinput (FileInput)
+
